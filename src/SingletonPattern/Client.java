f1863f9dcc24cefb8dc8a806e4ba88f59f1cc0e0
Snigdha Sanat <snigdhasanat@gmail.com>
2022-12-07 10:17:17 +0530

    Singleton pattern

---
+++ src/SingletonPattern/Client.java
@@ -0,0 +1,8 @@
+package SingletonPattern;
+
+public class Client {
+	public static void main(String[] args) {
+		SimpleSingletonClass object1=SimpleSingletonClass.getSingletonInstance1();
+		//SimpleSingletonClass object2=SimpleSingletonClass.getSingletonInstance2();
+	}//main
+}
---
+++ src/SingletonPattern/SimpleSingletonClass.java
@@ -0,0 +1,32 @@
+package SingletonPattern;
+
+public class SimpleSingletonClass {
+
+	//PRIVATE ctor
+	private SimpleSingletonClass(){};
+
+	private static SimpleSingletonClass theOnlyObject;
+
+	public static synchronized SimpleSingletonClass getSingletonInstance1(){
+		System.out.println("Calling getSingletonInstance1");
+		if(theOnlyObject==null){
+			theOnlyObject=new SimpleSingletonClass();
+		}
+		return theOnlyObject;
+	}
+
+//	public static  SimpleSingletonClass getSingletonInstance2(){
+//		System.out.println("Calling getSingletonInstance2");
+//		if(theOnlyObject==null){
+//			synchronized (SimpleSingletonClass.class){
+//				if(theOnlyObject==null){
+//					theOnlyObject=new SimpleSingletonClass();
+//				}//inner null check
+//			}//synchronised block
+//
+//		}//outer null check
+//		return theOnlyObject;
+//	}
+}//SimpleSingletonClass
+
+
